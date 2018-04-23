995dbd5f6eb58f206621c2c58af21a8b17f6331a
shaopeng1 <1377896554@qq.com>
2018-04-23 14:43:24 +0800

    Eureka可以启动，效果待定

--- spring_boot/src/main/java/com/example/sp/Application.java
+++ spring_boot/src/main/java/com/example/sp/Application.java
@@ -4,9 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableCaching //缓存策略
+@EnableEurekaServer
 @MapperScan("com.example.sp.mapper")//扫描
 public class Application {
 
