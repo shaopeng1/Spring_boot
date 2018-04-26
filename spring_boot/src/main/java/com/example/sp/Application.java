14ea4104633ea9c95c947e61543f2043288fdaec
shaopeng1 <1377896554@qq.com>
2018-04-26 14:03:45 +0800

    定时任务

--- spring_boot/src/main/java/com/example/sp/Application.java
+++ spring_boot/src/main/java/com/example/sp/Application.java
@@ -4,13 +4,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableCaching //缓存策略
-@EnableEurekaServer
+@EnableScheduling//开启定时任务
+//@EnableEurekaServer
 @MapperScan("com.example.sp.mapper")//扫描
-public class Application {
+public class Application { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
---
+++ spring_boot/src/main/java/com/example/sp/controller/MyTimer.java
@@ -0,0 +1,37 @@
+package com.example.sp.controller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * 定时任务 http://www.zslin.com/web/article/detail/21
+ * 
+ * @author shaopeng
+ *
+ */
+@Component
+public class MyTimer {
+	SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+
+	// 每3秒执行一次
+	@Scheduled(fixedRate = 3000)
+	public void timerRate() {
+		System.out.println(sdf.format(new Date()));
+	}
+
+	// 第一次延迟1秒执行，当执行完后3秒再执行
+	@Scheduled(initialDelay = 1000, fixedDelay = 3000)
+	public void timerInit() {
+		System.out.println("init : " + sdf.format(new Date()));
+	}
+
+	// 每天23点27分50秒时执行
+	@Scheduled(cron = "50 27 23 * * ?")
+	public void timerCron() {
+		System.out.println("current time : " + sdf.format(new Date()));
+	}
+
+}
